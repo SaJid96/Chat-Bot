fe81659c49f4c98c23ca4ae6c66f7023decd4764
SaJid96 <sajidhussain98@gmail.com>
2022-10-23 11:58:53 +0530

    Event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,64 @@
-const socket=io()
+const socket = io()
 
-socket.on('message',(message)=>{
+// Elements
 
-    console.log("this is a "+message+" message");
+const $messageForm = document.querySelector('#chatSend')
+const $messageFormInput = document.querySelector('input')
+const $messageFormButton = document.querySelector('button')
+
+socket.on('message', (message) => {
+
+    console.log("this message is from  server!!");
 })
 
-document.querySelector('#chatSend').addEventListener('submit',(e)=>{
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const message=e.target.elements.message
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
-    socket.emit('userMesssage',message);
+    const message = e.target.elements.message
+
+    socket.emit('userMesssage', message, (error) => {
+        setTimeout(() => {
+            $messageFormButton.removeAttribute('disabled')
+            $messageFormInput.value = ''
+            $messageFormInput.focus()
+
+        }, 2000)
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    });
 })
 
+
+const  $locationButton=document.querySelector('#send-location')
+
+$locationButton.addEventListener('click', () => {
+
+    if (!navigator.geolocation) {
+        return alert('Geo location not supported by Browser')
+    }
+
+    $locationButton.setAttribute('disabled','disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        var location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+        socket.emit('sendLocation', location, () => {
+            console.log('successfully location shared');
+            setTimeout(()=>{
+                $locationButton.removeAttribute('disabled')
+            },2000)
+
+        })
+
+
+    })
+
+
+})
